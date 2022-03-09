14a2e144f89466a9714193c70b45a6a577ffa80e
TULIP16 <tulip@cse.iitb.ac.in>
2022-03-09 10:31:32 +0530

    runner

--- runner.sh
+++ runner.sh
@@ -1,4 +1,4 @@
 python train_distill_learn_multilam_platt_scaling.py > platt_9.txt
-python train_distill_learn_multilam_all_train.python > all_train_9.txt
+python train_distill_learn_multilam_all_train.py > all_train_9.txt
 python temp_platt.py > platt_1.txt
 python temp_val.py > all_train_1.txt
